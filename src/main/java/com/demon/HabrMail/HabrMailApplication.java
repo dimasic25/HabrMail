df0beeac1552564d23eb83dee55edbd9e0d53de3
dimon <dimonbubyr@mail.ru>
2023-03-26 15:39:05 +0300

    Initial commit

---
+++ src/main/java/com/demon/HabrMail/HabrMailApplication.java
@@ -0,0 +1,13 @@
+package com.demon.HabrMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabrMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabrMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demon/HabrMail/HabrMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demon.HabrMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabrMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
